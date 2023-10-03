a84d36586a37d8466f911880e3bdfff9a6a672bc
ImAlka02 <ajiju.2000@hotmail.com>
2023-10-03 14:31:46 -0600

    Pixar No aparecen personajes referente a la peli

--- Peliculas/Controllers/PeliculasController.cs
+++ Peliculas/Controllers/PeliculasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Peliculas.Models.Entities;
 using Peliculas.Models.ViewModel;
 
@@ -16,6 +17,53 @@ namespace Peliculas.Controllers
             });
             
             return View(datos);
+        }
+
+        public IActionResult Detalles(string Id)
+        {
+            Id = Id.Replace("-", " ");
+
+            
+            PixarContext context = new();
+            var datos = context.Pelicula.OrderBy(x => x.Nombre).Where(x => x.Nombre.ToLower() == Id.ToLower()).FirstOrDefault();
+            if (datos == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //DetallesPeliculaViewModel vm = new()
+                //{
+                //    Id = datos.Id,
+                //    Nombre = datos.Nombre ?? "",
+                //    NombreOriginal = datos.NombreOriginal ?? "",
+                //    Fecha = datos.FechaEstreno.Value,
+                //    Descripcion = datos.Descripción ?? "",
+                //    Personajes = context.Pelicula.Select(x => new PersonajeModel
+                //    {
+                //        Id = x.Id,
+                //        Nombre = x.Nombre ?? ""
+                //    })
+                //};
+                var vm = context.Pelicula.Where(x => x.Nombre == Id).Select(x => new DetallesPeliculaViewModel()
+                {
+                    Id = datos.Id,
+                    Nombre = datos.Nombre ?? "",
+                    NombreOriginal = datos.NombreOriginal ?? "",
+                    Fecha = datos.FechaEstreno.Value,
+                    Descripcion = datos.Descripción ?? "",
+                    Personajes = x.Apariciones.Select(x=> new PersonajeModel()
+                    {
+                        Id = x.Id,
+                        Nombre = x.IdPersonajeNavigation.Nombre
+                    })
+                }).FirstOrDefault();
+                return View(vm);
+
+            };
+                
+            
+            
         }
     }
 }
---
+++ Peliculas/Models/ViewModel/DetallesPeliculaViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Peliculas.Models.ViewModel
+{
+    public class DetallesPeliculaViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string NombreOriginal { get; set; } = null!;
+        public DateOnly Fecha { get; set; }
+        public string Descripcion { get; set; } = null!;
+        public IEnumerable<PersonajeModel> Personajes { get; set; } = null!;
+    }
+    
+    public class PersonajeModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
